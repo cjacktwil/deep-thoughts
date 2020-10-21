5091d361f22d1fcdbc63f841ba6a8b8ba81b4e78
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-10-21 18:53:45 -0500

    set-up client-side server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -21,6 +22,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
