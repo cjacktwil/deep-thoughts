97a772808abbe9dd3e10ceaf2dba0eea984418c1
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-10-21 22:13:59 -0500

    completed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import { ApolloProvider } from '@apollo/react-hooks';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 import Home from './pages/Home';
 
 function App() {
@@ -14,15 +20,26 @@ function App() {
   });
   return (
     <ApolloProvider client={client}>
+    <Router>
       <div className="flex-column justify-flex-start min-100-vh">
         <Header />
         <div className="container">
-          <Home />
-        </div>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/signup" component={Signup} />
+    <Route exact path="/profile/:username?" component={Profile} />
+<Route exact path="/thought/:id" component={SingleThought} />
+
+    <Route component={NoMatch} />
+  </Switch>
+</div>
+
         <Footer />
       </div>
-    </ApolloProvider>
-  );
+    </Router>
+  </ApolloProvider>
+);
 }
 
 export default App;
