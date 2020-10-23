1eed088f29c6855e2319f4a19514312facc93f23
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-10-22 21:13:07 -0500

    added thought form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
-
+import ThoughtForm from '../components/ThoughtForm';
 import ThoughtList from '../components/ThoughtList';
-
-import { useQuery } from '@apollo/react-hooks';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -32,14 +33,30 @@ if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
       </h4>
     );
   }
+  
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
       <div className="flex-row mb-3">
-      <h2 className="bg-dark text-secondary p-3 display-inline-block">
-  Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-</h2>
-      </div>
+  <h2 className="bg-dark text-secondary p-3 display-inline-block">
+    Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+  </h2>
+
+  {userParam && (
+  <button className="btn ml-auto" onClick={handleClick}>
+    Add Friend
+  </button>
+)}
+</div>
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
@@ -54,6 +71,7 @@ if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
           />
         </div>
         </div>
+        <div className="mb-3">{!userParam && <ThoughtForm />}</div>
         </div>
   );
 };
